07e35b4ddd8ae598e7cddb63e5f60098b10cdec3
Joac <joaquin.gonzalez6693@gmail.com>
2025-05-06 17:27:29 -0300

    Renamed experiment phase

--- et_analysis.py
+++ et_analysis.py
@@ -61,7 +61,7 @@ def parse_asc_file(asc_file_path):
 
 edfs_path = paths.et_path + 'edfs/'
 bids_path = paths.main_path
-bids_name = 'DATA_bids'
+bids_name = 'DATA_bids_final'
 
 timestamps_onset = {}
 timestamps_offset = {}
@@ -70,38 +70,33 @@ for edf_file in os.listdir(edfs_path):
     subject_id = edf_file.split('_1')[0]
     asc_file_path = os.path.join(paths.et_path, subject_id, subject_id + '.asc')
     onset_subject, offset_subject = parse_asc_file(asc_file_path)
-    timestamps_onset[subject_id] = {'1': {'search': onset_subject}}
-    timestamps_offset[subject_id] = {'1': {'search': offset_subject}}
+    timestamps_onset[subject_id] = {'1': {'video': onset_subject}}
+    timestamps_offset[subject_id] = {'1': {'video': offset_subject}}
 
 bids_dataset_folder = pyx.dataset_to_bids(target_folder_path=bids_path, files_folder_path=edfs_path, dataset_name=bids_name)
 
 pyx.compute_derivatives_for_dataset(bids_dataset_folder=bids_dataset_folder, dataset_format="eyelink", detection_algorithm='eyelink',
                                     msg_keywords=['TTL_Signal_Value 80', 'TTL_Signal_Value 64', 'TTL_Signal_Value 96', 'Source_File cgi_video', 'TRIAL_VAR Start_Frame'],
-                                    start_times=timestamps_onset, end_times=timestamps_offset,
-                                    force_best_eye=False)
+                                    start_times=timestamps_onset, end_times=timestamps_offset)
 
 
 ##
 import pyxations as pyx
 import paths
 import pandas as pd
+from pyxations import Experiment
 
 edfs_path = paths.et_path + 'edfs/'
 bids_path = paths.main_path
 bids_name = 'DATA_bids'
 
-df = pd.read_feather(rf'{bids_path}{bids_name}_derivatives\sub-0001\ses-1\eyelink_events\fix.feather')
-df_msg = pd.read_feather(rf'{bids_path}{bids_name}_derivatives\sub-0001\ses-1\msg.feather')
-df_samples = pd.read_feather(rf'{bids_path}{bids_name}_derivatives\sub-0001\ses-1\samples.feather')
-df_sacc = pd.read_feather(rf'{bids_path}{bids_name}_derivatives\sub-0001\ses-1\eyelink_events\sacc.feather')
-
-df_samples['trial_number'].unique()
-
-
-from pyxations import Experiment
 exp = Experiment(rf"{bids_path}{bids_name}")
 exp.load_data("eyelink")
 
-exp.drop_poor_or_non_calibrated_trials(0.5)
+exp.drop_trials_with_nan_threshold("search",0.1)
+exp.collapse_fixations(80.0)
+exp.filter_fixations()
+exp.plot_multipanel(True)
+exp.plot_scanpaths(screen_height=1080, screen_width=1920)
 
 exp.plot_calib_data()
